79e7beb7a87b179cf660f9cb3ac8373ecadeba05
Sally Sghair <sallysghair1@gmail.com>
2022-02-25 22:21:35 +0000

    skills page styled

--- src/common/Skills.js
+++ src/common/Skills.js
@@ -3,23 +3,73 @@ import React from 'react'
 
 function Skills () {
   return (
-    <section className="container" id="skills">
-      <button className="glass-card">HTML</button>
-      <button className="glass-card">CSS3</button>
-      <button className="glass-card">JavaScript</button>
-      <button className="glass-card">Sass</button>
-      <button className="glass-card">React</button>
-      <button className="glass-card">Bootstrap</button>
-      <button className="glass-card">Python</button>
-      <button className="glass-card">Node.js</button>
-      <button className="glass-card">Express</button>
-      <button className="glass-card">MongoDB</button>
-      <button className="glass-card">Django</button>
-      <button className="glass-card">TablePlus</button>
-      <button className="glass-card">Insomnia</button>
-
+    <section className="skills-body">
+      <div className="container skills">
+        <div className="box">
+          <span></span>
+          <div className="content">
+            <h2>Front-End Skills</h2>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Sass</li>
+            <li>Bootstrap</li>
+            <li>Bulma</li>          
+          </div>
+        </div>
+        <div className="box">
+          <span></span>
+          <div className="content">
+            <h2>Back-End Skills</h2>
+            <li>Python</li>
+            <li>Node.js</li>
+            <li>Express</li>
+            <li>MongoDB</li>
+            <li>Django</li>
+            <li>TablePlus</li>
+            <li>Insomnia</li>  
+          </div>
+        </div>
+        <div className="box">
+          <span></span>
+          <div className="content">
+            <h2>Soft Skills</h2>
+            <li>Collabrative</li>
+            <li>Effecient</li>
+            <li>Comprehensive</li>
+            <li>problem-solving</li>
+            <li>Communication</li>
+            <li>Adaptability</li>
+            <li>Fluent in professional Arabic</li>  
+          </div>
+        </div>
+      </div>
 
     </section>
+  
+
+
+
+
+
+  // <section className="container" id="skills">
+  //   <button className="glass-card">HTML</button>
+  //   <button className="glass-card">CSS3</button>
+  //   <button className="glass-card">JavaScript</button>
+  //   <button className="glass-card">Sass</button>
+  //   <button className="glass-card">React</button>
+  //   <button className="glass-card">Bootstrap</button>
+  //   <button className="glass-card">Python</button>
+  //   <button className="glass-card">Node.js</button>
+  //   <button className="glass-card">Express</button>
+  //   <button className="glass-card">MongoDB</button>
+  //   <button className="glass-card">Django</button>
+  //   <button className="glass-card">TablePlus</button>
+  //   <button className="glass-card">Insomnia</button>
+
+
+  // </section>
     
   )
 }
